6e5197cf6b648942e334358540a451457c31d639
João Igor <joao.melo@esx.com.br>
2020-05-03 16:43:48 -0300

    formatando arquivo .tf

--- main.tf
+++ main.tf
@@ -1,15 +1,15 @@
 provider "azurerm" {
-    features = {}
+  features = {}
 }
 
-resource "azurerm_resource_group"  "my_resourcegroup" {
-    name = "resource_group_exemplo"
-    location = "Brazil South"
+resource "azurerm_resource_group" "my_resourcegroup" {
+  name     = "resource_group_exemplo"
+  location = "Brazil South"
 }
 
 resource "azurerm_application_insights" "my_applicationinsights" {
-    name = "myapplicationinsights"
-    resource_group_name = azurerm_resource_group.my_resourcegroup.name
-    location = azurerm_resource_group.my_resourcegroup.location
-    application_type = "Web"
+  name                = "myapplicationinsights"
+  resource_group_name = azurerm_resource_group.my_resourcegroup.name
+  location            = azurerm_resource_group.my_resourcegroup.location
+  application_type    = "Web"
 }
